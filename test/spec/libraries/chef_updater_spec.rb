00db100d50471ecc0df496ca992ff99c5cf6f769
John Bellone <jbellone@bloomberg.net>
2015-10-26 09:57:47 -0400

    Fixes the testing harnesses.

--- test/spec/libraries/chef_updater_spec.rb
+++ test/spec/libraries/chef_updater_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'poise_boiler/spec_helper'
-require_relative File.expand_path('../../../libraries/chef_updater')
+require_relative '../../../libraries/chef_updater'
 
 describe ChefUpdaterCookbook::Resource::ChefUpdater do
   step_into(:chef_updater)
--- test/spec/recipes/default_spec.rb
+++ test/spec/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe_recipe 'chef-updater::default' do
-  cached(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+describe 'chef-updater::default' do
+  cached(:chef_run) { ChefSpec::SoloRunner.converge('chef-updater::default') }
 
   context 'with all default attributes' do
     it { expect(chef_run).to run_chef_updater('chef').with(package_version: '12.5.1-1') }
@@ -14,10 +14,10 @@ describe_recipe 'chef-updater::default' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
         node.set['chef-updater']['package_version'] = '13.33-7'
-      end.converge(described_recipe)
+      end.converge('chef-updater::default')
     end
 
-    it { expect(chef_run).to create_chef_updater('chef').with(package_version: '13.33-7') }
+    it { expect(chef_run).to run_chef_updater('chef').with(package_version: '13.33-7') }
     it 'converges successfully' do
       chef_run
     end
