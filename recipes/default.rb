293eca43b9fc8e782e7c13ce06437671fb7831c7
Shahul Hameed <skhajamohid1@bloomberg.net>
2015-10-27 00:12:12 -0400

    Set timeout and package_options correctly

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,4 +9,7 @@ chef_updater node['chef-updater']['package_name'] do
   package_version node['chef-updater']['package_version']
   package_checksum node['chef-updater']['package_checksum']
   package_source node['chef-updater']['package_source']
+  timeout node['chef-updater']['timeout'] if node['chef-updater']['timeout']
+  package_options node['chef-updater']['package_options'] if
+    node['chef-updater']['package_options']
 end.run_action(:run)
