2033a371d725cea3be80b6fdb3e7a065a7a2af8a
John Bellone <jbellone@bloomberg.net>
2016-04-13 10:00:20 -0400

    Fix default value for package name on windows.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,11 @@
 #
 # Copyright 2015-2016, Bloomberg Finance L.P.
 #
-default['chef-updater']['package_name'] = 'chef'
+if platform?('windows')
+  default['chef-updater']['package_name'] = 'chef-client'
+else
+  default['chef-updater']['package_name'] = 'chef'
+end
+
 default['chef-updater']['package_version'] = '12.8.1-1'
 default['chef-updater']['package_source'] = nil
